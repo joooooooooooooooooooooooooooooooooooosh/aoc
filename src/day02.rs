3469b01492a36da41b724d9d94d033942d9a64ad
Josh Harcombe <joshharc@gmail.com>
2021-12-02 20:45:58 +1100

    day02 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -23,15 +23,44 @@ pub fn part1(input: String) -> Option<i32> {
             _ => unreachable!(),
         }
 
-        let distance: u16 = line.split(" ").nth(1)?.parse().unwrap();
-        *change += direction * distance as i32;
+        let distance: i32 = line.split(" ").nth(1)?.parse().unwrap();
+        *change += direction * distance;
     }
 
-    let result = horizontal * vertical;
-    Some(result)
+    Some(horizontal * vertical)
 }
 
-pub fn part2(input: String) -> Option<u16> {
-    let result = 0;
-    Some(result)
+pub fn part2(input: String) -> Option<i32> {
+    let mut horizontal = 0;
+    let mut vertical = 0;
+    let mut aim = 0;
+
+    let lines = input.lines();
+    for line in lines {
+        let movement = line.split(" ").nth(0)?;
+        let distance: i32 = line.split(" ").nth(1)?.parse().unwrap();
+
+        let change: &mut i32;
+        let direction;
+        match movement {
+            "forward" => {
+                change = &mut horizontal;
+                direction = 1;
+                vertical += aim * distance;
+            },
+            "down" => {
+                change = &mut aim;
+                direction = 1;
+            },
+            "up" => {
+                change = &mut aim;
+                direction = -1;
+            },
+            _ => unreachable!(),
+        }
+
+        *change += direction * distance;
+    }
+
+    Some(horizontal * vertical)
 }
