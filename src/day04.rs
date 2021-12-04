1ad6bd283c6af61abfd3a154fe047c3ece9f8441
Josh Harcombe <joshharc@gmail.com>
2021-12-05 00:07:47 +1100

    day04 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -29,9 +29,9 @@ pub fn part1(input: String) -> Option<u64> {
             }
         }
 
-        let winning_board = check_winner(&boards);
-        if winning_board.is_some() {
-            result = calculate_score(winning_board?, num);
+        let winning_boards = check_winners(&boards);
+        if winning_boards.len() > 0 {
+            result = calculate_score(winning_boards[0].clone(), num);
             break;
         }
     }
@@ -39,7 +39,8 @@ pub fn part1(input: String) -> Option<u64> {
     Some(result)
 }
 
-fn check_winner(boards: &Vec<Vec<Vec<(u8, bool)>>>) -> Option<Vec<Vec<(u8, bool)>>> {
+fn check_winners(boards: &Vec<Vec<Vec<(u8, bool)>>>) -> Vec<Vec<Vec<(u8, bool)>>> {
+    let mut winning_boards: Vec<Vec<Vec<(u8, bool)>>> = Vec::new();
     for board in boards {
         let mut cols = vec![0; BOARD_SIZE];
         for row in board {
@@ -49,16 +50,16 @@ fn check_winner(boards: &Vec<Vec<Vec<(u8, bool)>>>) -> Option<Vec<Vec<(u8, bool)
                 }
             }
             if row.iter().all(|(_, b)| *b) {
-                return Some(board.clone());
+                winning_boards.push(board.clone());
             }
         }
 
         if cols.into_iter().any(|n| n as usize == BOARD_SIZE) {
-            return Some(board.clone());
+            winning_boards.push(board.clone());
         }
     }
 
-    None
+    winning_boards
 }
 
 fn calculate_score(board: Vec<Vec<(u8, bool)>>, winning_num: u8) -> u64 {
@@ -75,6 +76,36 @@ fn calculate_score(board: Vec<Vec<(u8, bool)>>, winning_num: u8) -> u64 {
 }
 
 pub fn part2(input: String) -> Option<u64> {
-    let result = 0;
+    let mut input = input.lines();
+    let numbers = input.next()?
+                       .split(',')
+                       .map(|s| s.parse::<u8>().unwrap());
+
+    let mut boards: Vec<Vec<Vec<(u8, bool)>>> = Vec::new();
+    while input.next().is_some() {
+        let mut board = Vec::new();
+        for _ in 0..BOARD_SIZE {
+            let row: Vec<u8> = parse_string(input.next()?).ok()?;
+            board.push(row.into_iter().map(|num| (num, false)).collect());
+        }
+        boards.push(board);
+    }
+
+    let mut result = 0;
+    for num in numbers {
+        for board in &mut boards {
+            for row in board {
+                row.iter_mut()
+                    .filter(|(n, _)| *n == num)
+                    .for_each(|(_, b)| *b = true);
+            }
+        }
+
+        for winner in check_winners(&boards) {
+            result = calculate_score(winner.clone(), num);
+            boards.retain(|x| x != &winner);
+        }
+    }
+
     Some(result)
 }
