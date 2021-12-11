b9e88da0d171abff089138d88251a53d6b611d72
Josh Harcombe <joshharc@gmail.com>
2021-12-12 10:34:37 +1100

    day09 part 2

--- src/day09.rs
+++ src/day09.rs
@@ -1,11 +1,10 @@
 const BASE: u32 = 10;
 
 pub fn part1(input: String) -> Option<u32> {
-    // surely there's a better way than using charindices but i cbb
     let heightmap = input.lines()
                         .map(|line|
-                            line.char_indices()
-                                .filter_map(|x| (x.1).to_digit(BASE))
+                            line.chars()
+                                .filter_map(|x| x.to_digit(BASE))
                                 .collect::<Vec<_>>())
                         .collect::<Vec<_>>();
 
@@ -34,7 +33,62 @@ pub fn part1(input: String) -> Option<u32> {
                 .sum())
 }
 
-pub fn part2(input: String) -> Option<u64> {
-    let result = 0;
-    Some(result)
+pub fn part2(input: String) -> Option<u32> {
+    let mut heightmap = input.lines()
+                            .map(|line|
+                                line.chars()
+                                    .filter_map(|x| x.to_digit(BASE))
+                                    .collect::<Vec<_>>())
+                            .collect::<Vec<_>>();
+
+    let mut basins: Vec<u32> = Vec::new();
+    for (i, row) in heightmap.clone().iter().enumerate() {
+        for (j, &num) in row.iter().enumerate() {
+            if i != 0 && num >= heightmap[i-1][j] {
+                continue;
+            }
+            if j != 0 && num >= heightmap[i][j-1] {
+                continue;
+            }
+            if i != heightmap.len() - 1 && num >= heightmap[i+1][j] {
+                continue;
+            }
+            if j != row.len() - 1 && num >= heightmap[i][j+1] {
+                continue;
+            }
+
+            basins.push(calculate_size(&mut heightmap, i, j));
+        }
+    }
+
+    basins.sort();
+    basins.reverse();
+    Some(basins.iter()
+            .take(3)
+            .fold(1, |acc, x| acc * x))
+}
+
+fn calculate_size(heightmap: &mut Vec<Vec<u32>>, row: usize, col: usize) -> u32 {
+    if heightmap[row][col] == 9 {
+        return 0;
+    }
+
+    let mut size = 1;
+    // prevent double counting
+    heightmap[row][col] = 9;
+
+    if row != 0 {
+        size += calculate_size(heightmap, row - 1, col);
+    }
+    if col != 0 {
+        size += calculate_size(heightmap, row, col - 1);
+    }
+    if row != heightmap.len() - 1 {
+        size += calculate_size(heightmap, row + 1, col);
+    }
+    if col != heightmap[row].len() - 1 {
+        size += calculate_size(heightmap, row, col + 1);
+    }
+
+    size
 }
