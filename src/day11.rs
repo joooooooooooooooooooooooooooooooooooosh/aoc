3d7afa33e43006b5874b43ba2ae627afe8f5afed
Josh Harcombe <joshharc@gmail.com>
2021-12-13 10:18:29 +1100

    day11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -24,7 +24,6 @@ pub fn part1(input: String) -> Option<u64> {
             new_flash = false;
             for (i, row) in octopi.clone().iter().enumerate() {
                 for (j, octopus) in row.iter().enumerate() {
-                    // this breaks because something could go from 9-11 in one go
                     if octopus.0 > 9 && !octopus.1 {
                         new_flash = true;
                         flashes += 1;
@@ -72,7 +71,77 @@ pub fn part1(input: String) -> Option<u64> {
     Some(flashes)
 }
 
-pub fn part2(input: String) -> Option<u64> {
-    let result = 0;
-    Some(result)
+pub fn part2(input: String) -> Option<usize> {
+    let mut octopi = input.lines()
+                          .map(|line|
+                                line.chars()
+                                    .map(|c| (c.to_digit(BASE).unwrap(), false))
+                                    .collect::<Vec<_>>())
+                          .collect::<Vec<_>>();
+
+    for i in 1.. {
+        let mut new_flash = true;
+        let mut all_flash = true;
+        octopi.iter_mut().for_each(|line|
+            line.iter_mut().for_each(|octopus|{
+                // bool represents if the octopus has flashed that round
+                octopus.0 += 1;
+                octopus.1 = false;
+            })
+        );
+
+        while new_flash {
+            new_flash = false;
+            for (i, row) in octopi.clone().iter().enumerate() {
+                for (j, octopus) in row.iter().enumerate() {
+                    if octopus.0 > 9 && !octopus.1 {
+                        new_flash = true;
+                        octopi[i][j].0 += 1;
+                        octopi[i][j].1 = true;
+                        
+                        if i > 0 {
+                            octopi[i - 1][j].0 += 1;
+                            if j > 0 {
+                                octopi[i - 1][j - 1].0 += 1;
+                            }
+                            if j < row.len() - 1 {
+                                octopi[i - 1][j + 1].0 += 1;
+                            }
+                        }
+                        if i < octopi.len() - 1 {
+                            octopi[i + 1][j].0 += 1;
+                            if j > 0 {
+                                octopi[i + 1][j - 1].0 += 1;
+                            }
+                            if j < row.len() - 1 {
+                                octopi[i + 1][j + 1].0 += 1;
+                            }
+                        }
+                        if j > 0 {
+                            octopi[i][j - 1].0 += 1;
+                        }
+                        if j < row.len() - 1 {
+                            octopi[i][j + 1].0 += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        octopi.iter_mut().for_each(|line|
+            line.iter_mut().for_each(|octopus|{
+                if octopus.0 > 9 {
+                    octopus.0 = 0;
+                } else {
+                    all_flash = false;
+                }
+            })
+        );
+
+        if all_flash {
+            return Some(i);
+        }
+    }
+
+    None
 }
