f2c84f367791b843c99fd2c966698b709645df46
Josh Harcombe <joshharc@gmail.com>
2021-12-15 12:11:26 +1100

    day13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -42,7 +42,47 @@ pub fn part1(input: String) -> Option<usize> {
 }
 
 pub fn part2(input: String) -> Option<u64> {
-    let result = 0;
-    Some(result)
+    let mut dots: Vec<Vec<bool>> = vec![vec![false; GRID_SIZE]; GRID_SIZE];
+    let mut input = input.lines();
+
+    while let Some(line) = input.next() {
+        if line == "" {
+            break;
+        }
+        let point: Vec<usize> = line.split(",")
+                              .map(|x| x.parse().unwrap())
+                              .collect();
+        dots[point[1]][point[0]] = true;
+    }
+
+    while let Some(mut line) = input.next() {
+        line = line.split(' ').skip(2).next()?;
+        let direction = line.chars().next()?;
+        let fold: usize = line.split('=').skip(1).next()?.parse().unwrap();
+
+        for (i, row) in dots.clone().iter().enumerate() {
+            for (j, dot) in row.iter().enumerate() {
+                if *dot {
+                    if i > fold && direction == 'y' {
+                        dots[i][j] = false;
+                        dots[(fold - (i - fold))][j] = true;
+                    } else if j > fold && direction == 'x' {
+                        dots[i][j] = false;
+                        dots[i][(fold - (j - fold))] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    // no way i'm gonna bother parsing this when i have eyes
+    for row in dots.iter().take(6) {
+        for dot in row.into_iter().take(40) {
+            print!("{}", if *dot { '#' } else { '.' });
+        }
+        println!();
+    }
+
+    None
 }
 
