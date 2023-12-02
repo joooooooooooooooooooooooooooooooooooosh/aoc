5a4919259cdd7fd0ebd20c5c2e414166ef8352d6
Josh Harcombe <joshharc@gmail.com>
2023-12-02 23:59:54 +1100

    day 2

---
+++ 2023/2/2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# part 1
+# < input grep -Ev "((1[3-9]|[2-9].) red|(1[4-9]|[2-9].) green|(1[5-9]|[2-9].) blue)" | sed -E 's/Game ([0-9]+): .*/\1/' | awk '{s+=$1} END{print s}'
+
+# part 2
+< input sed 's/Game .*:/echo "/; s/$/" | tr ",;" "\\n" | sort -rn | sort -sk2 | uniq -f1 | awk "BEGIN{s=1} {s*=\\$1} END{print s}"/' | sh | awk "{s+=\$1} END{print s}"
