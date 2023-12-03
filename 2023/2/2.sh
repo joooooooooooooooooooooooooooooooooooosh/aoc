03049c163ec0c327889eee1d0fe1bde10d763fd0
Josh Harcombe <joshharc@gmail.com>
2023-12-03 12:09:33 +1100

    day 2 alternative

--- 2023/2/2.sh
+++ 2023/2/2.sh
@@ -5,3 +5,6 @@
 
 # part 2
 < input sed 's/Game .*:/echo "/; s/$/" | tr ",;" "\\n" | sort -rn | sort -sk2 | uniq -f1 | awk "BEGIN{s=1} {s*=\\$1} END{print s}"/' | sh | awk "{s+=\$1} END{print s}"
+
+# less worse (but slower somehow) alternative
+# < input xargs -I{} sh -c "echo '{}' | cut -d: -f2 | tr ',;' '\\n' | sort -rn | sort -sk2 | uniq -f1 | awk 'BEGIN{s=1} {s*=\$1} END{print s}'" | awk "{s+=\$1} END{print s}"
