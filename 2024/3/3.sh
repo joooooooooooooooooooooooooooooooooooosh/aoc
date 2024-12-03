8f7bb49af66f942c9bcdcf4f2e2195d4882b777e
Josh Harcombe <jharcombe@atlassian.com>
2024-12-03 16:18:03 +1100

    3 p2

--- 2024/3/3.sh
+++ 2024/3/3.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
 grep -Eo 'mul\([0-9]+,[0-9]+\)' input | sed 's/mul(\(.*\),\(.*\))/echo $((\1 * \2))/' | sh | awk '{s+=$1} END{print s}'
+
+# Part 2
+tr '\n' ' ' < input | sed "s/do()/\n/g; s/don't()/\n#####/g" | grep -v ^##### | grep -Eo 'mul\([0-9]+,[0-9]+\)' | sed 's/mul(\(.*\),\(.*\))/echo $((\1 * \2))/' | sh | awk '{s+=$1} END{print s}'
