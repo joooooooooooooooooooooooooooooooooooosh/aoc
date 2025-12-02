47bece79e8ed9566798fe58ad7cc6eb0974138d6
Josh Harcombe <jharcombe@atlassian.com>
2025-12-02 16:43:25 +1100

    attempted 2024 d5p2

--- 2024/5/5.sh
+++ 2024/5/5.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-sed -E -f <(grep \| input | sed -E 's_(.*)\|(.*)_/(^|,)\2,(.*,)?\1(,|$)/d;_') <(grep , input) | sed -E -e ':l' -e 's/^[^,]+,(.*),[^,]+$/\1/;tl' | awk '{s+=$1} END{print s}'
+# sed -E -f <(grep \| input | sed -E 's_(.*)\|(.*)_/(^|,)\2,(.*,)?\1(,|$)/d;_') <(grep , input) | sed -E -e ':l' -e 's/^[^,]+,(.*),[^,]+$/\1/;tl' | awk '{s+=$1} END{print s}'
+
+# Part 2
+# gsed for MacOS
+# sed -E -f <(grep \| input | gsed -E -e 's_(.*)\|(.*)_s/(^|,)(\2),(.*,)?(\1)(,|$)/\\1\\4,\\3\\2\\5/;ts_; $ats\nd\n:s' | tee /dev/stdout | tac | sed 1,2d | tac) <(grep , input) | sed -E -e ':l' -e 's/^[^,]+,(.*),[^,]+$/\1/;tl' | awk '{s+=$1} END{print s}'
+sed -E -f <(grep \| input | gsed -E -e 's_(.*)\|(.*)_s/(^|,)(\2),(.*,)?(\1)(,|$)/\\1\\4,\\3\\2\\5/;ts_; $ad\n:s' | tee /dev/stdout | tac | sed 1,2d | tac) <(grep , input) | sed -E -e ':l' -e 's/^[^,]+,(.*),[^,]+$/\1/;tl' | awk '{s+=$1} END{print s}'
